53f013190a022c3068652a64d4c195e7fcb7269d
aws <90727515+awsaz-git@users.noreply.github.com>
2025-12-19 16:23:32 +0300

    Backend setup (#6)
    * Back-end Setup
    
    * Setup Database
    
    * Backend Auth Setup
    
    ---------
    
    Co-authored-by: Ha-banna <113101100+Ha-banna@users.noreply.github.com>

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,7 +1,17 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    MONGODB_URI: str
+    MONGODB_DB: str
+
+    SUPERTOKENS_CONNECTION_URI: str
+    SUPERTOKENS_API_KEY: str
+
+    API_DOMAIN: str
+    API_BASE_PATH: str
+
+    WEBSITE_DOMAIN: str
+    WEBSITE_BASE_PATH: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
