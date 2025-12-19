b9edda0ebef70f466985e5a09ff06046c6927bce
Ha-banna <113101100+Ha-banna@users.noreply.github.com>
2025-12-19 14:28:29 +0300

    Added 3 new pages (#5)
    Co-authored-by: Hamza Albanna <hamza.albanna@maqsam.com>

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LandingPage from '../components/LandingPage.vue'
 import Dashboard from '../components/Dashboard.vue'
+import CreateCV from '../components/CreateCV.vue'
+import Login from '../components/Login.vue'
+import RequestJoin from '../components/RequestJoin.vue'
 
 const routes = [
   {
@@ -9,9 +12,24 @@ const routes = [
     component: LandingPage
   },
   {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/request-join',
+    name: 'RequestJoin',
+    component: RequestJoin
+  },
+  {
     path: '/dashboard',
     name: 'Dashboard',
     component: Dashboard
+  },
+  {
+    path: '/create-cv',
+    name: 'CreateCV',
+    component: CreateCV
   }
 ]
 
